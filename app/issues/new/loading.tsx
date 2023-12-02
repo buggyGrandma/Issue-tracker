98ee1210c084b8a3cf5001ffc117cdf826c7f1b4
buggyGrandma <alireza.kh3221@gmail.com>
2023-12-02 23:39:32 +0330

    Refactor: Organize import statements

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,11 +1,5 @@
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
-import { Callout, TextField, Button, Box } from "@radix-ui/themes";
-import { register } from "module";
-import error from "next/error";
-import { Controller } from "react-hook-form";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
+import { Box } from "@radix-ui/themes";
 
 const LoadingNewIssuePage = () => {
   return (
